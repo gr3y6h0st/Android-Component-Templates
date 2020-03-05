76d831e8b909f6fb9e1a1911c1141c6b2528f80f
gr3y6h0st <jayvy19@gmail.com>
2020-03-04 23:32:40 -0800

    RecyclerView Template

---
+++ app/src/main/java/com/templates/application/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.templates.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v
+7.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/templates/application/RecyclerViewAdapter.kt
@@ -0,0 +1,4 @@
+package com.templates.application
+import android.support.v7.widget.RecyclerView
+class RecyclerViewAdapter  {
+}
